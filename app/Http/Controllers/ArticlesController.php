ae6dc5c3513e6d2835d540daa7599a7a2048e645
Yacin abboub <y.abboub@respawn-agency.com>
2015-11-27 22:05:43 +0100

    Add ArticleController

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Article;
+use App\Category;
+use Intervention\Image\ImageManagerStatic as Image;
+
+class ArticlesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::get();
+        return view('articles.index',compact('articles'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::lists('title','id');
+        return view('articles.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Requests\EditArticleRequest $request)
+    {
+        $img = Image::make($request->images);
+        $mime = $img->mime();  //edited due to updated to 2.x
+            if ($mime == 'image/jpeg')
+                $extension = '.jpg';
+            elseif ($mime == 'image/png')
+                $extension = '.png';
+            elseif ($mime == 'image/gif')
+                $extension = '.gif';
+            else
+                $extension = '';
+        $img->save(public_path('images/'.$request->title.$extension));
+        $article = new Article();
+        $article = Article::create($request->except('images'));
+        $article->images = $request->title.$extension;
+        $article->save();
+        return redirect(route('articles.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.show',compact('article'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $article = Article::findOrfail($id);
+        $categories = Category::lists('title','id');
+        return view('articles.edit',compact('article','categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Requests\EditArticleRequest $request, $id)
+    {
+        $article = Article::findOrfail($id);
+        $article->update($request->except('images'));
+        if($request->images){
+            $img = Image::make($request->images);
+            $mime = $img->mime();  //edited due to updated to 2.x
+            if ($mime == 'image/jpeg')
+                $extension = '.jpg';
+            elseif ($mime == 'image/png')
+                $extension = '.png';
+            elseif ($mime == 'image/gif')
+                $extension = '.gif';
+            else
+                $extension = '';
+            $img->save(public_path('images/'.$request->title.$extension));
+            $article->images = $request->title.$extension;
+            $article->save();
+            return redirect(route('articles.index'));
+        }
+        return redirect(route('articles.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return redirect(route('categories/article/',$article->category_id ));
+    }
+}
